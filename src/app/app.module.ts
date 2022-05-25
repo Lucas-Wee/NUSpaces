2ae28326369ffa97102d813830e5f3330e530129
Tan Jin Waye <e0694446@u.nus.edu>
2022-05-25 13:10:48 +0800

    Commit skeleton for home page, no functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { PostComponent } from './component/post/post.component';
 
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // angular material
@@ -15,9 +15,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 
+
+import { MatCardModule } from '@angular/material/card';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
@@ -28,6 +33,7 @@ import { MatDividerModule } from '@angular/material/divider';
     MatButtonModule,
     MatIconModule,
     MatDividerModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
