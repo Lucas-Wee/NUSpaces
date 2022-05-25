b781ac89bf373f6c2a4c2569264be9cc0111477f
Tan Jin Waye <e0694446@u.nus.edu>
2022-05-25 17:40:36 +0800

    Added Post component, updated Home to use Post accordingly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,6 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
-
-
 import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
