d06093236416f86f040347b51abb076e3f1957ac
Tan-Jin-Waye <92251448+Tan-Jin-Waye@users.noreply.github.com>
2022-05-30 15:49:19 +0800

    Add files via upload

---
+++ src/app/data/posts_test.ts
@@ -0,0 +1,40 @@
+import { Post } from './Post';
+
+export const POSTS: Post[] = [
+    {
+        image: "https://mustsharenews.com/wp-content/uploads/2021/06/NUS.jpeg",
+        comment: "Default Comment",
+        category: 0,
+        
+        ID: "00000",
+        date: "01/01/2000",
+        time: "00:00",
+
+        userName: "Person A",
+        userAvatar: "https://g.foolcdn.com/editorial/images/677442/shiba-inu-dog_CGVWTf7.jpg"
+    },
+    {
+        image: "https://d.newsweek.com/en/full/280341/nus-university-town.jpg",
+        comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        category: 1,
+        
+        ID: "00001",
+        date: "25/02/2002",
+        time: "23:59",
+
+        userName: "Person B",
+        userAvatar: "https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg"
+    },
+    {
+        image: "https://siamnus.github.io/website/rsz_cover.jpg",
+        comment: "This is a reminder to make a text cutoff when the comments get too big",
+        category: 2,
+        
+        ID: "00002",
+        date: "28/02/2020",
+        time: "02:02",
+
+        userName: "Person C",
+        userAvatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Maned_Wolf_by_Trisha_7.jpg/800px-Maned_Wolf_by_Trisha_7.jpg"
+    }
+]
